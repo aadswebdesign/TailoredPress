fcd24e8029b912c7bc83dd4ef869063e8a6dc575
Aad <aad.pouw@gmail.com>
2022-12-11 09:21:40 +0100

    some editions

--- index.php
+++ index.php
@@ -11,5 +11,5 @@ if( ! defined('TP_MULTI_SITE')) define('TP_MULTI_SITE', false);//todo set false 
 require_once(ABSPATH.'tp_autoload.php');
 if(ABSPATH){
 	new tp_autoload();
-    echo new \TP_Content\Themes\DefaultTheme\Theme_Index();
+    echo new \TP_Content\Themes\ThemePanel();
 }else die;
